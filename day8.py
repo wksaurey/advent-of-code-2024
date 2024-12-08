8970a7e01c0897d6d9ce5260475f826586720404
kolter <wksaurey@gmail.com>
2024-12-08 13:45:27 -0700

    buggy

--- day8.py
+++ day8.py
@@ -9,6 +9,43 @@ def main():
     antennas = get_antennas(grid)
     print(antennas)
 
+    print()
+    antinodes = get_antinodes(antennas, grid)
+
+    for antinode in antinodes:
+        x, y = antinode
+
+    print(f'Antinodes: {len(antinodes)}')
+
+def get_antinodes(antennas, grid):
+    antinodes = []
+    for antenna_type in antennas:
+        for i in range(len(antennas[antenna_type])):
+            for j in range(i+1, len(antennas[antenna_type])):
+                x1, y1 = antennas[antenna_type][i]
+                x2, y2 = antennas[antenna_type][j]
+                dx = x1 - x2
+                dy = y1 - y2
+
+                new_antinodes = [(x1 - dx, y1 - dy), (x1 + dx, y1 + dy)]
+
+                for antinode in new_antinodes:
+                    x, y = antinode
+                    if (x < 0 or x >= len(grid[0]) or 
+                            y < 0 or y >= len(grid)):
+                        continue
+                    if antinode in antinodes:
+                        continue
+
+                    print(f'({x}, {y})')
+                    if grid[y][x] == '.':
+                        grid[y][x] = '#'
+
+                    print_grid(grid)
+                    antinodes.append(antinode)
+
+    return antinodes
+
 def get_antennas(grid):
     antennas = {}
     for y, line in enumerate(grid):

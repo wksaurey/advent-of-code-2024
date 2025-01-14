927d3b2f94e70be140d035ab7325058bd906f137
wksaurey <wksaurey@gmail.com>
2025-01-14 10:19:21 -0700

    day 9 part 1

--- day9.py
+++ day9.py
@@ -2,16 +2,20 @@ from util import read_stripped_lines
 
 def main():
     filename = 'bin/day9_test.txt'
+    # filename = 'bin/day9.txt'
 
     disk_map = read_stripped_lines(filename, nums=True)[0]
-
-    print(disk_map)
 
     disk = []
     is_file = True
 
     id = 0
     for value in disk_map:
+        if value == 0:
+            if is_file:
+                id += 1
+            is_file = not is_file
+            continue
         if is_file:
             disk.append(File(id, value))
             id += 1
@@ -19,38 +23,87 @@ def main():
             disk.append(File('.', value))
         is_file = not is_file
 
+    defrag(disk)
     print_disk(disk)
 
-    defrag(disk)
+    checksum = 0
+    index = 0
+    for file in disk:
+        if file.is_space():
+            break
+
+        for _ in range(file.len):
+            # print(f'{file.id} * {index} = {file.id * index}')
+            checksum += index * file.id
+            index += 1
+
+    print(f'Checksum: {checksum}')
+    # 258894222412 too low
 
 def defrag(disk):
-    file_index, file = get_file(disk) 
-    space_index, space = get_space(disk)
-
-    print(f'File: {file.id}')
-    print(f'Space: {space.id}')
 
     while True:
-        if file.len == 0:
-            disk.pop(file_index)
-            file_index, file = get_file(disk) 
-        if space.len == 0:
-            disk.pop(space_index)
-            space_index, space = get_space(disk)
-            file_index -= 1
+        print_disk(disk)
+        # print(f'Disk len: {len(disk)}')
+        # print(f'Space len {disk[-1].len}')
+        file_index, file = get_file(disk) 
+        space_index, space = get_space(disk)
+        print(f'File: {file.id}')
 
         if space_index > file_index:
-            return
+            break
+
+        if file.len > space.len:
+            if disk[-1].is_space():
+                disk[-1].len += space.len
+            else:
+                disk.insert(len(disk), File(space.id, space.len))
+            space.id = file.id
+            file.len -= space.len
+        elif space.len > file.len:
+            disk.insert(space_index, file)
+            if disk[-1].is_space():
+                disk[-1].len += space.len
+            else:
+                disk.insert(len(disk), File(space.id, file.len))
+            space.len -= file.len
+            file_index, file = get_file(disk) 
+            disk.pop(file_index)
+        elif file.len == space.len:
+            space.id = file.id
+            if disk[-1].is_space():
+                disk[-1].len += space.len
+            else:
+                disk.insert(len(disk), File(space.id, file.len))
+            file_index, file = get_file(disk) 
+            disk.pop(file_index)
+
+        if disk[-1].is_space() and disk[-2].is_space():
+            disk[-2].len += disk[-1].len
+            disk.pop()
+
+# def consolidate_space(disk):
+#     index = 0
+    
+#     while index < len(disk)-1:
+#         file0 = disk[index]
+#         file1 = disk[index+1]
+#         if file0.is_space() and file1.is_space():
+#             file0.len += file1.len
+#             disk.pop(index+1)
+#         else:
+#             index += 1
+
 
 def get_file(disk):
     for index in range(len(disk)-1, -1, -1):
         file = disk[index]
-        if file.id != '.':
+        if not file.is_space():
             return index, file
 
 def get_space(disk):
     for index, file in enumerate(disk):
-        if file.id == '.':
+        if file.is_space():
             return index, file
     
 def print_disk(disk):
@@ -62,6 +115,9 @@ class File:
     def __init__(self, id, len):
         self.id = id
         self.len = len
+
+    def is_space(self):
+        return self.id == '.'
 
     def print(self):
         print(str(self.id) * self.len, end='')
